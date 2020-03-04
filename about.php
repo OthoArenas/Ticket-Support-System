01e054eaa1423a99eb72060d06630b40e4ba71be
Othoniel Eduardo Salazar Arenas <othosalazar@gmail.com>
2020-03-04 13:58:56 -0600

    Fully functional ticket support system

---
+++ about.php
@@ -0,0 +1,57 @@
+<?php
+$title = "Información | ";
+include "head.php";
+?>
+
+<div class="text-center" role="main">
+    <!-- page content -->
+    <div class="">
+        <div class="page-title">
+            <br>
+            <div class="row justify-content-center">
+                <div class="col-xs-12 col-md-8">
+                    <h1>Acerca del sistema</h1>
+                    <hr>
+                    <p><strong><a target="_blank" href="http://dsignstudio.com.mx">Othoniel Salazar</a></strong> <strong><a target="_blank" href="http://dsignstudio.com.mx">(D-Sign Studio)</a></strong> creador de <a>GOLFO TICKET</a>.</p>
+
+                    <br><br>
+
+                    <h2>GOLFO TICKET - <small>Sistema de Tickets de Soporte</small></h2>
+                    <p>GOLFO TICKET es un sistema de Tickets de soporte de para servicio técnico y mantenimiento correctivo/preventivo.</p>
+
+                    <br><br>
+                    <hr>
+                    <h1 style="text-align: center;">GOLFO TICKET</h1>
+                    <hr>
+                    <h3>Descripción</h3>
+                    <p>GOLFO TICKET es un sistema para gestionar tickets de soporte mediante departamentos, categorias y prioridades, ideal para todo tipo de negocios, desarrollo de proyectos, gestion personal, etc. Sistema desarrollado con PHP y MySQL.</p>
+                    <p>GOLFO TICKET es un sistema de tickets de soporte en el que puedes crear un perfil, crear tickets, departamentos y categorias, generar reportes y mucho más.</p>
+
+                    <h3>Módulos</h3>
+                    <ul>
+                        <li><b>Usuarios</b>: Agregar, editar y eliminar usuarios.</li>
+                        <li><b>Tickets</b>: Crear Tickets para soporte técnico.</li>
+                        <li><b>Departamentos</b>: Agregar, editar y eliminar departamentos.</li>
+                        <li><b>Perfiles</b>: Los usuarios pueden actualizar su perfil</li>
+                        <li><b>Categorias</b>: Agregar, editar y eliminar categorias.</li>
+                        <li><b>Reportes</b>: Los usuarios pueden generar reportes.</li>
+                    </ul>
+
+                    <br>
+                    <p>Si deseas contratar el servicio de soporte técnico para el sistema de GOLFO TICKET debes enviar un E-mail a: othoniel.salazara@dsignstudio.com.mx, con el asunto "Soporte GOLFO TICKET", en donde se detalle el tipo de ayuda que se desea recibir. Luego se responderá mediante una cotización con el costo del servicio, forma de pago y tiempo de entrega.</p>
+                    <p>El sistema es de código abierto, puede ser utilizado para empresas o uso personal.</p>
+
+                    <h3>Apóyanos</h3>
+                    <p>Hay muchas formas en las que puedes apoyarnos para seguir creando contenido, proyectos y actualizaciones a los sistemas</p>
+
+                    <br><br>
+                    <hr>
+                    <a href="http://dsignstudio.com.mx" target="_blank"><img src="images/logo.png" alt="D-Sign Studio Logo" class="text-center" id="about_logo"></a>
+                    <h3 style="text-align: center;"><a href="http://dsignstudio.com.mx" target="_blank">D-Sign Studio | Web Solutions</a></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><!-- /page content -->
+
+<?php include "footer.php" ?>
